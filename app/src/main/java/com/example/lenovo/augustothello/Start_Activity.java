a30d4a87226c453182a9174003f59b0fe1ace25a
Tanishq Goel <tanishqgoel@gmail.com>
2017-09-06 00:20:22 +0530

    Logic Done

---
+++ app/src/main/java/com/example/lenovo/augustothello/Start_Activity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.augustothello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_);
+    }
+}
